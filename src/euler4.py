787ad4c905394aa7c30dbf6343e47a24e61aa57f
William Mead <will@Williams-iMac.local>
2020-04-05 11:33:36 +0100

    Added euler4.py and euler5.py

---
+++ src/euler4.py
@@ -0,0 +1,37 @@
+"""
+A palindromic number reads the same both ways. The largest palindrome
+made from the product of two 2-digit numbers is 9009 = 91 x 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+import itertools
+
+def is_palindrome(number):
+    string = str(number)
+    return string == string[::-1]
+
+def largest_palindrome_1():
+    for i in range(100, 1000):
+        for j in range(100, 1000):
+            product = i * j
+            if is_palindrome(product):
+                largest_palindrome = product
+
+    return largest_palindrome
+
+
+def largest_palindrome_2():
+    three_digit_nums = range(100, 1000)
+
+    for x, y in itertools.product(three_digit_nums, three_digit_nums):
+        product = x * y
+        if is_palindrome(product):
+            largest_palindrome = product
+
+    return largest_palindrome
+
+
+our_range = range(100, 1000)
+palindromes = [x * y for x, y in itertools.product(our_range, our_range) if is_palindrome(x * y)]
+
+print(largest_palindrome_2())
---
+++ src/euler5.py
@@ -0,0 +1,32 @@
+"""
+2520 is the smallest number that can be divided by each of
+the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible
+by all of the numbers from 1 to 20?
+"""
+
+def divisible_1(number, max):
+    for test_factor in range(max, 0, -1):
+        if number % test_factor: # the result has a remainder
+            return False
+    return True
+
+
+def divisible_2(number, max):
+    for test_factor in range(1, max+1):
+        if number % test_factor: # the result has a remainder
+            return False
+    return True
+
+
+searching = True
+max = 20
+current_num = max
+
+while searching:
+    if divisible_2(current_num, max):
+        searching = False
+        print(current_num)
+    else:
+        current_num += max
